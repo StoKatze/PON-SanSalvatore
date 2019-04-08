3c31fc6618dc9b5b88226bf5df336db556d3e0b7
Christian Vassallo <44880451+StoKatze@users.noreply.github.com>
2019-04-08 13:02:17 +0200

    Update server

--- ClientServer/server.py
+++ ClientServer/server.py
@@ -14,14 +14,17 @@ while True:
 		data = conn.recv(1024) # Riceve dati
 		rcvd = data.decode()
 		if rcvd == 'inizio':
+			time.sleep(30) # Attesa di 30 secondi prima di iniziare la riproduzione
 			print('Avvio')
+			lunghezzaVideoSecondi = 30 # Durata del video in secondi (approssimata per eccesso)
+			player = subprocess.Popen(['omxplayer', '-b' , 'test.mp4']) # Apre il player
+			time.sleep(lunghezzaVideoSecondi) # Mette in attesa il programma fin quando non termina la riproduzione
+			player.kill() # Chiude il player
 			break
 		if rcvd == 'fine':
 			print('Termino')
+			player.kill()
 			break
 		elif not data: break
 	conn.send(data.encode('utf-8'))
 	conn.close()
-
-
-
