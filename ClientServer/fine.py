5561423a66491a9372a5b2db64687dc1d174deb6
Christian Vassallo <44880451+StoKatze@users.noreply.github.com>
2019-04-08 15:19:53 +0200

    Updated fine script
    This is the final version. No further changes other than IPs should be changed.

--- ClientServer/fine.py
+++ ClientServer/fine.py
@@ -1,11 +1,11 @@
 import socket
 import time
 
-HOST = '127.0.0.1'    # Indirizzo del server
+HOST = '192.169.203.81'    # Indirizzo del server
 PORT = 50007          # La stessa porta usata dal server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Crea l'oggetto Socket
 s.connect((HOST, PORT)) # Connessione al server
-stringa = 'fine'
+stringa = 'fine' # Messaggio da inviare
 s.send(stringa.encode('utf-8')) # Manda la stringa codificata in utf-8
-data = s.recv(1024)
-s.close()
+data = s.recv(1024) # Riceve il messaggio di chiusura connessione
+s.close() # Chiude la connessione
