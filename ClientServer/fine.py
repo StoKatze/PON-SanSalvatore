8e3e28c0f39847584be28c264815ce60ae735ab2
Christian Vassallo <44880451+StoKatze@users.noreply.github.com>
2019-04-08 15:21:03 +0200

    Updated fine script
    This is the actual final version. No further changes other than IPs should be changed.

--- ClientServer/fine.py
+++ ClientServer/fine.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import sys
 
 HOST = '192.169.203.81'    # Indirizzo del server
 PORT = 50007          # La stessa porta usata dal server
@@ -9,3 +10,4 @@ stringa = 'fine' # Messaggio da inviare
 s.send(stringa.encode('utf-8')) # Manda la stringa codificata in utf-8
 data = s.recv(1024) # Riceve il messaggio di chiusura connessione
 s.close() # Chiude la connessione
+sys.exit() # Chiude lo script
