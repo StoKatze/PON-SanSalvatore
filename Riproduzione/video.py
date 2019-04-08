34e6c3dcb76e13d704cb9d83e6d434c1b54c0428
Christian Vassallo <44880451+StoKatze@users.noreply.github.com>
2019-04-08 15:33:18 +0200

    Added final version of video.py
    This is the final version. Only file name should be changed.

--- Riproduzione/video.py
+++ Riproduzione/video.py
@@ -2,12 +2,12 @@
 # N.B. Il video non viene mostrato tramite VNC, ma solo sulla porta HDMI
 # !!! N.B. Per l'audio occorre impostare una modalita' CEA e non DMT !!! Vedi: raspi-config > Advanced > Resolution
 
-import subprocess # Utilizzato per aprire il player in un sottoprocesso controllabile
-import time # Utilizzato per lo sleep
+import subprocess
+import time
 import sys
 
-lunghezzaVideoSecondi = 30 # Durata del video in secondi (approssimata per eccesso)
-player = subprocess.Popen(['omxplayer', '-b' , 'test.mp4']) # Apre il player
-time.sleep(lunghezzaVideoSecondi) # Mette in attesa il programma fin quando non termina la riproduzione
+#lunghezzaVideoSecondi = 30 # Durata del video in secondi (approssimata per eccesso)
+player = subprocess.check_output(['omxplayer', '-b' , 'test.mp4']) # Apre il player
+#time.sleep(lunghezzaVideoSecondi) # Mette in attesa il programma fin quando non termina la riproduzione
 player.kill() # Chiude il player
-sys.exit()
+sys.exit() # Termina lo script
