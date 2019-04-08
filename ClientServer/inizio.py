a8791f0c00364534a3a187debf6d72860f813a66
Christian Vassallo <44880451+StoKatze@users.noreply.github.com>
2019-04-08 15:24:35 +0200

    Updated inizio.py
    This is the final version. No further changes other than IPs should be changed.

--- ClientServer/inizio.py
+++ ClientServer/inizio.py
@@ -1,11 +1,13 @@
 import socket
 import time
+import sys
 
-HOST = '127.0.0.1'    # Indirizzo del server
-PORT = 50007          # La stessa porta usata dal server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = '192.169.203.81' # Indirizzo del server
+PORT = 50007 # La stessa porta usata dal server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Crea l'oggetto socket
 s.connect((HOST, PORT)) # Connessione al server
-stringa = 'inizio'
+stringa = 'inizio' # Messaggio da inviare
 s.send(stringa.encode('utf-8')) # Manda la stringa codificata in utf-8
-data = s.recv(1024)
-s.close()
+data = s.recv(1024) # Riceve il messaggio di chiusura connessione
+s.close() # Chiude la connessione
+sys.exit() # Chiude lo script
