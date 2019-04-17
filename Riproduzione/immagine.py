560f0bc2d41f2edebdf209b75cc172a8b061c7fb
Christian Vassallo <44880451+StoKatze@users.noreply.github.com>
2019-04-17 08:48:21 +0200

    Add files via upload

--- Riproduzione/immagine.py
+++ Riproduzione/immagine.py
@@ -1,15 +1,12 @@
-# Script utilizzato solo per mostrare uno schermo nero quando le proiezioni non sono attive
-
 #!/usr/bin/python
 import pygame
 import time
-import sys
 
-picture = pygame.image.load("test.jpg") # Carica l'immagine
+picture = pygame.image.load("nero.jpg") #Carica l'immagine
 picture = pygame.transform.scale(picture,(1280,720))
-panel = pygame.display.set_mode((1,1), pygame.FULLSCREEN) # Imposta la dimensione dell'immagine e la modalita' a schermo intero
-pygame.mouse.set_visible(False) # Nasconde il cursore
-panel.blit(picture,(0,0)) # Imposta il primo punto dell'immagine
-pygame.display.flip() # Aggiorna la finestra 
-time.sleep(10000) # Timeout esagerato
-sys.exit() # Interrompe il programmma
+panel = pygame.display.set_mode((1,1), pygame.FULLSCREEN)
+pygame.mouse.set_visible(False) #Nasconde il cursore
+panel.blit(picture,(0,0))
+pygame.display.flip()
+time.sleep(10000) #Timeout esagerato
+
